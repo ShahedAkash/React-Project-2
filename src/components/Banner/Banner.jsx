e0c51acfe05ab5a81a88de92d9d0ca4d240ed51f
ShahedAkash <baker.akash48@gmail.com>
2023-09-27 19:33:31 +0600

    styled search btn in Banner jsx file

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,13 +2,15 @@
 const Banner = () => {
     return (
         <div className="h-[85vh] flex flex-col justify-center items-center">
-            <div className="-z-10 opacity-10 h-[110vh] absolute">
+            <div className="-z-10 opacity-10 h-[100vh] -mt-12 absolute">
                 <img className="h-[100%] w-full" src="https://i.ibb.co/fFymVY1/Rectangle-4281.png)" />
             </div>
             <div className="absolute flex flex-col justify-center items-center">
-                <h2 className="text-5xl font-bold">I Grow By Helping People In Need</h2>
-                <div className="my-10">
-                    <input type="text" placeholder="Type here" className="input input-bordered input-md w-full max-w-xs" />
+                <h2 className="text-xl md:text-2xl lg:text-4xl font-bold">I Grow By Helping People In Need</h2>
+
+                <div className="my-10 flex">
+                    <input type="text" placeholder="Search here....." className="input input-bordered input-md w-full max-w-xs rounded-r-none" />
+                    <button className="btn bg-red-500 text-white rounded-l-none border-red-500">Search</button>
                 </div>
             </div>
         </div>
--- src/components/Donation/Donation.jsx
+++ src/components/Donation/Donation.jsx
@@ -10,7 +10,7 @@ const Donation = ({ donation }) => {
             <div className="card card-compact bg-base-100 shadow-xl">
                 <figure><img className="w-full h-[50vh] md:h-[30vh] lg:h-[30vh]" src={img} /></figure>
                 <div style={{backgroundColor: color}}  className="card-body rounded-b-xl">
-                    <h6 style={{color: text_color, backgroundColor: type_bg_color}} className="text-xs p-1 rounded-lg">{type}</h6>
+                    <h6 style={{color: text_color, backgroundColor: type_bg_color}} className="text-xs p-1 px-2 rounded-lg w-fit">{type}</h6>
                     <h2 style={{color: text_color}} className="card-title">{title}</h2>
                 </div>
             </div>
